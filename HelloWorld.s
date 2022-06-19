911b209f7c582466c0e77821eb9ee3aeb42239fb
Paul Shanks <paulshankster@gmail.com>
2022-06-18 21:15:10 -0500

    updated comments

--- HelloWorld.s
+++ HelloWorld.s
@@ -13,13 +13,13 @@
 _start: mov	X0, #1		// 1 = StdOut
 	ldr	X1, =helloworld // string to print
 	mov	X2, #13		// length of out string
-	mov	X8, #64		//Linux write system call
-	svc	0		// Call to Linux to output the string
+	mov	X8, #64		// Linux system call: 
+	svc	0		// Software interupt
 
 // Setup the parameters to exit the program
 // and the call Linux to do it.
 	mov	X0, #0	// Use 0 return code
-	mov	X8, #93	// Service code 93 terminates
+	mov	X8, #93	// Linux System call: Service code 93 terminates
 	svc	0	// Call Linux to terminate
 
 .data
